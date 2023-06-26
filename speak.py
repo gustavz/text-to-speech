6a76b7aeafc826b20174f14a2970ffbd4784d4d7
Gustav von Zitzewitz <gustav.vonzitzewitz@datarobot.com>
2023-06-26 21:22:29 +0200

    add voices and more args

--- speak.py
+++ speak.py
@@ -1,8 +1,10 @@
 import argparse
 import os
 
+import pyttsx3
 from gtts import gTTS
 from playsound import playsound
+from voices import PYTTSX3_VOICES
 
 TEXT = "text.txt"
 SPEECH = "speech.mp3"
@@ -16,6 +18,9 @@ if __name__ == "__main__":
     parser.add_argument("-t", "--text", default=TEXT)
     parser.add_argument("-s", "--speech", default=SPEECH)
     parser.add_argument("-m", "--mute", action="store_true")
+    parser.add_argument("-e", "--engine", default="gtts", choices=["gtts", "pyttsx3"])
+    parser.add_argument("-v", "--voice", default="Good News", choices=PYTTSX3_VOICES.keys())
+    parser.add_argument("-r", "--rate", default=150)
     args = parser.parse_args()
 
     # read file if it is a path
@@ -23,14 +28,17 @@ if __name__ == "__main__":
         with open(args.text, "r") as file:
             args.text = file.read()
 
-    tts = gTTS(args.text, lang="en")
-    tts.save(args.speech)
-    if not args.mute:
-        playsound(args.speech)
-
-    # pyttsx3 is buggy and can't save to file on macOS
-    # import pyttsx3
-    # engine = pyttsx3.init()
-    # engine.say(args.text)
-    # engine.runAndWait()
-    # engine.save_to_file(args.text, args.speech)
+    if args.engine == "gtts":
+        tts = gTTS(args.text, lang="de")
+        tts.save(args.speech)
+        if not args.mute:
+            playsound(args.speech)
+    elif args.engine == "pyttsx3":
+        # pyttsx3 is buggy and can't save to file on macOS
+        engine = pyttsx3.init()
+        engine.setProperty('voice', PYTTSX3_VOICES[args.voice])
+        engine.setProperty('rate', int(args.rate))
+        if not args.mute:
+            engine.say(args.text)
+            engine.runAndWait()
+        engine.save_to_file(args.text, args.speech)
